ba324a42fdcc686f39b07637db53577e491f9fcb
OlivierL-KRG <olivier.larue@krgcorporate.com>
2020-02-28 15:43:13 +0100

    Refactoring.

--- app/js/services/notification-service.js
+++ app/js/services/notification-service.js
@@ -1,28 +1,28 @@
 class NotificationService {
     constructor() {
-        this.element = document.getElementById('notifications');
+        this.element = document.getElementById('js-notifications');
     }
 
     notify(type, message) {
         switch (type) {
             case 'SAVING':
-                this.element.className = '';
-                this.element.classList.add('saving');
+                this.element.className = 'notifications';
+                this.element.classList.add('notifications--saving');
 
                 this.element.innerHTML = message;
                 break;
 
             case 'SUCCESS':
-                this.element.className = '';
-                this.element.classList.add('success');
+                this.element.className = 'notifications';
+                this.element.classList.add('notifications--success');
 
                 this.element.innerHTML = message;
                 this._resetState();
                 break;
 
             case 'FAILURE':
-                this.element.className = '';
-                this.element.classList.add('failure');
+                this.element.className = 'notifications';
+                this.element.classList.add('notifications--failure');
 
                 this.element.innerHTML = message;
                 this._resetState();
@@ -35,7 +35,7 @@ class NotificationService {
 
     _resetState() {
         setTimeout(() => {
-            this.element.className = '';
+            this.element.className = 'notifications';
             this.element.innerHTML = '';
         }, 2000);
     }
