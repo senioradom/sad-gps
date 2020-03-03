f0ecd476031e9142faa915d6ef6a1e61183099d4
OlivierL-KRG <olivier.larue@krgcorporate.com>
2020-03-03 11:09:04 +0100

    Simplify the notification service.

--- app/js/services/notification-service.js
+++ app/js/services/notification-service.js
@@ -3,32 +3,12 @@ class NotificationService {
         this.element = document.getElementById('js-notifications');
     }
 
-    notify(type, message, cssClass) {
-        switch (type) {
-            case 'SAVING.START':
-            case 'SAVING.SUCCESS':
-            case 'SAVING.FAILURE':
-            case 'LABELS.VALIDATION.FAILURE':
-                return;
-                // eslint-disable-next-line no-unreachable
-                this.element.className = 'notifications';
-                this.element.classList.add(`notifications--${cssClass}`);
+    notify(message, cssClass) {
+        this.element.className = 'notifications';
+        this.element.classList.add(`notifications--${cssClass}`);
 
-                this.element.innerHTML = message;
-                this._resetState();
-                break;
-
-            case 'REPLAY.NO_DATA':
-                this.element.className = 'notifications';
-                this.element.classList.add(`notifications--${cssClass}`);
-
-                this.element.innerHTML = message;
-                this._resetState();
-                break;
-
-            default:
-                break;
-        }
+        this.element.innerHTML = message;
+        this._resetState();
     }
 
     _resetState() {
