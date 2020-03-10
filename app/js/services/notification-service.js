e55b9a92b734763e09fe32682f2141339e3952fd
OlivierL-KRG <olivier.larue@krgcorporate.com>
2020-03-10 18:02:36 +0100

    Add ability to set the notification system duration.

--- app/js/services/notification-service.js
+++ app/js/services/notification-service.js
@@ -1,21 +1,22 @@
 class NotificationService {
     constructor() {
         this.element = document.getElementById('js-notifications');
+        this.TIMEOUT = 3500;
     }
 
-    notify(message, cssClass) {
+    notify(message, cssClass, timeout) {
         this.element.className = 'notifications';
         this.element.classList.add(`notifications--${cssClass}`);
 
         this.element.innerHTML = message;
-        this._resetState();
+        this._resetState(timeout || this.TIMEOUT);
     }
 
-    _resetState() {
+    _resetState(timeout) {
         setTimeout(() => {
             this.element.className = 'notifications';
             this.element.innerHTML = '';
-        }, 3500);
+        }, timeout);
     }
 }
 
